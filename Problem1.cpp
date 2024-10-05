ff8fa58cbb75a1bbf36a084eb3e281a1c0d98d11
reem-abdou <146747432+reem-abdou@users.noreply.github.com>
2024-10-05 11:01:30 +0300

    Update Problem1.cpp

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
 #include <cstring>
+#include <cctype>
 
 using namespace std;
 int main()
 {
-    string sent;
-    string ans;
-    getline(cin,sent);
+    char st[1000];
+    string ans="";
+    cout << "Please, Enter your sentence:\n";
+    cin.getline (st,1000, '.');
+    string sent = st;
+    sent+='.';
     bool first=false;
-    for(int i = 0; i<sent.size(); i++){
-        if(isalpha(sent[i]) && first==false){
+    int i=0;
+    for (int i = 0 ; i < sent.size() ; i++){
+        if(isalpha(sent[i]) && !first){
             ans+=toupper(sent[i]);
             first=true;
         }
-        else if(sent[i]==' '){
-            if(!ans.empty()){
+        else if(sent[i]==' ' || sent[i]=='\n'){
+            while(sent[i+1]==' ' || sent[i+1]=='\n'){
+                i++;
+            }
+            if(!ans.empty() && (isalpha(sent[i+1])||isdigit(sent[i+1]))){
                 ans+=' ';
             }
-            while(sent[i+1]==' '){
-                i++;
-                continue;
-            }
         }
-        
         else{
             ans+=tolower(sent[i]);
         }
